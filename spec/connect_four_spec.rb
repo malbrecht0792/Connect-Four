abade7b6ceb044e55eb0a8c7db203dbdd237d0a5
Marcel <malbrecht0792@gmail.com>
2017-11-29 21:49:50 -0600

    Added tests for Board class

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -19,3 +19,43 @@ describe Player do
     end
   end
 end
+
+describe Board do
+  let (:my_board) do
+    Board.new
+  end
+
+  let (:my_player) do
+    Player.new('player1', 'X')
+  end
+
+  describe 'initialize' do
+    context 'given a new instance of class Board, Board.spaces.length' do
+      it "returns the board's size" do
+        expect(my_board.spaces.length).to eql(42)
+      end
+    end
+  end
+
+  describe '#update_board' do
+    context 'given player1 and space to move as 10' do
+      before do
+        my_board.update_board(my_player, 10)
+      end
+      it "returns 'X' for space 10 on the board" do
+        expect(my_board.spaces[10]).to eql('X')
+      end
+    end
+  end
+
+  describe '#undo_move' do
+    context 'given space to undo as 10' do
+      before do
+        my_board.undo_move(10)
+      end
+      it "returns '-' for space 10 on the board" do
+        expect(my_board.spaces[10]).to eql('-')
+      end
+    end
+  end
+end
